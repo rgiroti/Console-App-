51e5011f0875c2630f6be895c71482f6af5657a7
Ravi Giroti <rgiroti.sfo@gmail.com>
2011-07-05 23:47:12 -0700

    Initial commit

---
+++ src/HelloWorld.java
@@ -0,0 +1,15 @@
+/**
+ * Created by IntelliJ IDEA.
+ * User: ravi
+ * Date: 7/5/11
+ * Time: 11:39 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class HelloWorld {
+
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+
+}
